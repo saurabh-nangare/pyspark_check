0b89a3a0896b58b4fb52feba69e04379c22f042c
Saurabh <saurabhnangare97@gmail.com>
2024-06-17 18:43:38 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.getOrCreate()
+
+df = spark.read.csv(r'D:\Hu_Edge\dataset.txt',header=True,sep=',')
+df.write.parquet(r'D:\Hu_Edge\datawritecheck',mode= 'append')

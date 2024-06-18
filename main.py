1a27bcd10bb31fbc1ac6c548a8afd2a9d4f19133
Saurabh <saurabhnangare97@gmail.com>
2024-06-18 10:37:29 +0530

    commiting initial changes

--- main.py
+++ main.py
@@ -4,3 +4,5 @@ spark = SparkSession.builder.getOrCreate()
 
 df = spark.read.csv(r'D:\Hu_Edge\dataset.txt',header=True,sep=',')
 df.write.parquet(r'D:\Hu_Edge\datawritecheck',mode= 'append')
+
+print('git')
